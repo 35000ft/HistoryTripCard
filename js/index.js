72345664a8512145f618d91e1bc1f9ea40e14167
35000ft <35000feet2@gmail.com>
2022-12-12 11:36:18 +0800

    v0.0.1

---
+++ js/index.js
@@ -0,0 +1,76 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        province: '',
+        cities: [],
+        provincesOptions: [],
+        cityOptions: [],
+        rawData: [],
+        totalCities: [],
+        citiesString: ''
+    },
+    mounted() {
+        this.loadCities();
+    },
+    methods: {
+        loadCities() {
+            const url = './assets/ChineseCities.json';
+            console.log("loading " + url);
+            axios.get(url).then(res => {
+                this.rawData = res.data.provinces;
+                console.log("load " + url + " successfully");
+                this.initProvincesOptions();
+            }, () => {
+                console.log('Load ' + url + ' Error!');
+            });
+        },
+        initProvincesOptions() {
+            this.rawData.forEach((data) => {
+                let item = {};
+                item.value = data.provinceName;
+                item.label = data.provinceName;
+                this.provincesOptions.push(item);
+            });
+        },
+        initCityOptions() {
+            let province = this.province;
+            if (province == '') {
+                return;
+            }
+            this.cities = [];
+            this.cityOptions = [];
+            let cities = this.rawData.find(item => item.provinceName === province).citys;
+            console.log(cities);
+            cities.forEach((city) => {
+                let item = {};
+                item.value = city.cityName;
+                item.label = city.cityName;
+                this.cityOptions.push(item);
+            });
+        },
+        addCities() {
+            this.cities.forEach((city) => {
+                city = this.province + city;
+                this.totalCities.push(city);
+            })
+            this.province = '';
+            this.cities = [];
+            this.cityOptions = [];
+            this.updateCitiesString();
+        },
+        updateCitiesString() {
+            this.totalCities = Array.from(new Set(this.totalCities))
+            this.totalCities.sort((a, b) => a.localeCompare(b));
+            this.citiesString = '';
+            this.totalCities.forEach((item) => {
+                if (this.citiesString.length > 0) {
+                    this.citiesString += ',';
+                }
+                this.citiesString += item;
+            });
+        }
+
+
+    }
+
+});
